0d70ab23979a62da9a9794a5b107385ad7c0fec9
Naveen <Naveen@DESKTOP-I1FJI67.attlocal.net>
2022-08-07 00:06:20 -0500

    First commit

---
+++ Encapsulation/src/employee.java
@@ -0,0 +1,24 @@
+
+
+public class employee {
+
+	private String Name;
+	private int ID;
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	
+	
+	
+
+}
---
+++ Encapsulation/src/main.java
@@ -0,0 +1,17 @@
+
+
+public class main {
+
+	public static void main(String[] args) {
+		employee s = new employee();
+		s.setName("Naveen");
+		s.setID(375);
+		System.out.println(s.getName());
+		System.out.println(s.getID());
+		
+		
+		
+
+	}
+
+}
